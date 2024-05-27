cb58a2180257e17e0544fbac8024899fdd2f2840
nvidia <kenric.tan@pavesolutions.com>
2024-05-27 17:26:11 +0800

    fixed

---
+++ build/CMakeFiles/sample_app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_app.dir/src/main.cpp.o"
+  "sample_app"
+  "sample_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main() {
     cv::namedWindow("Display", cv::WINDOW_GUI_NORMAL | cv::WINDOW_NORMAL );
     cv::setWindowProperty("Display", cv::WND_PROP_FULLSCREEN, cv::WINDOW_GUI_EXPANDED);
-    cv::VideoCapture camera(0)
+    cv::VideoCapture camera(0);
     camera.set(cv::CAP_PROP_FRAME_WIDTH, 1600);
     camera.set(cv::CAP_PROP_FRAME_HEIGHT, 1200);
     ImageUnwrapper unwrapper("../src/config.txt");
